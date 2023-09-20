4db8d7ffc67f8d0ecb7e9e0767a01e0e91631a7e
Anderson <118360226+brAndersu@users.noreply.github.com>
2023-09-20 16:55:58 -0300

    Update PID_V2.ino

--- PID_V2.ino
+++ PID_V2.ino
@@ -6,10 +6,17 @@ int sensorWeights[NUM_SENSORS] = {-40, -30, -20, -10, 10, 20, 30, 40}; // Pesos 
 int minValues[NUM_SENSORS]; // Vetor para armazenar os valores mínimos
 int maxValues[NUM_SENSORS]; // Vetor para armazenar os valores máximos
 
+const int motorEsquerdo = 5;
+const int motorDireito = 6;
+const int IN1 = 7;
+const int IN2 = 8;
+const int IN3 = 9;
+const int IN4 = 10;
+
 // Parâmetros do PID
 double Setpoint, Input, Output;
 double errSum = 0, lastErr = 0;
-double Kp=0.4, Ki=0, Kd=0;
+double Kp=1.3, Ki=0.2, Kd=2;
 
 void setup() {
   Serial.begin(9600);
@@ -22,10 +29,16 @@ void setup() {
   }
 
   // Inicializa o PID
-  Setpoint = 5; // Defina o valor de referência aqui
+  Setpoint = 0; // Defina o valor de referência aqui
+  // pinMode(13, LOW);
+  // delay(3000);
+  // pinMode(13, HIGH);
+  // delay(2000);
+  // // Inicia a calibração
+  // calibrateSensors();
+  // pinMode(13, LOW);
+  // delay(2000);
 
-  // Inicia a calibração
-  calibrateSensors();
 }
 
 void loop() {
@@ -62,15 +75,16 @@ void calibrateSensors() {
 double readLinePosition() {
   double somaPonderada = 0;
   double soma = 0;
-  
+  double limmax = 1023;
   for (int i = 0; i < NUM_SENSORS; i++) {
     int value = analogRead(sensorPins[i]);
 
-    somaPonderada += (value*sensorWeights[i]);
-    soma += value;
+    int valor = limmax - value;
+    somaPonderada += (valor*sensorWeights[i]);
+    soma += valor;
     
 
-    Serial.print(value);
+    Serial.print(valor);
     Serial.print(" ; ");
   }
   
@@ -94,5 +108,30 @@ void Compute() {
 }
 
 void controlCar(double output) {
-   // função para controlar o carro com base na saída do PID.
+  int speed = 65; // Velocidade base
+  int leftSpeed = speed + abs(output);
+  int rightSpeed = speed + abs(output);
+
+  if (output > 0) {
+    rightSpeed -= 2*abs(output);
+  } else {
+    leftSpeed -= 2*abs(output);
+  }
+
+  // Garante que a velocidade esteja entre 0 e 140
+  leftSpeed = constrain(leftSpeed, 0, 150);
+  rightSpeed = constrain(rightSpeed, 0, 150);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(motorEsquerdo, leftSpeed);
+  analogWrite(motorDireito, rightSpeed);
+
+  Serial.print("Motor Esquerdo: ");
+  Serial.print(leftSpeed);
+  Serial.print(" Motor Direito: ");
+  Serial.print(rightSpeed);
+  Serial.print(" ");
 }
