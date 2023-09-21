fd2507c02f4451a7d50e3127c13f3d9ea8a12f5c
Anderson <118360226+brAndersu@users.noreply.github.com>
2023-09-21 09:34:10 -0300

    Reformulação do codigo.
    Adicionado calibração, conversão de analogico para digital e registro de ultima posição (util caso o robo saia completamente da linha)

--- Seguidor_PID.ino
+++ Seguidor_PID.ino
@@ -1,98 +1,226 @@
-// Pinos dos motores
-const int ENA = 5;
-const int ENB = 6;
-const int in1 = 7;
-const int in2 = 8;
-const int in3 = 9;
-const int in4 = 10;
+// Pinos ponte h
+#define IN1   11
+#define IN2   3
+#define IN3   10
+#define IN4   9
+#define IR  7
 
-// Parâmetros do PID
-double Setpoint, Input, Output;
-double errSum = 0, lastErr = 0;
-double Kp=0.3, Ki=0.000008, Kd=0.000008;
+// Variaveis controle de velocidade para módulos ponte-h drv8833 e l9110s
+int PWMA;
+int PWMB;
 
-void Compute() {
-    double error = Setpoint - Input;
-    errSum += error;
-    double dErr = error - lastErr;
+// Arrays que guardam os valores de leitura dos sensores para casos específicos 
+int leituraPista[8]; // para calibração
+int leituraLinha[8]; // para calibração
+int sensores[8]; // para leitura analógica
+int digital[8];  // para conversão de analógico a digital
 
-    Output = Kp * error + Ki * errSum + Kd * dErr;
+const int pinosSensores[8] = {A0, A1, A2, A3, A4, A5, A6, A7};
 
-    lastErr = error;
+// Pista = 0, pista branca com linha preta
+// Pista = 1, pista preta com linha branca
+int pista = 1;
+
+// Array que, após a calibração, guarda os valores individuais de funcionamento de cada sensor
+int linha[8];
+
+// Variáveis para calcular a posição do robô em relação à linha
+long int somaPonderada, soma, posicao, posicaoLinha, ultimaPosicao;
+
+// Variáveis PID
+float KP = 0.4;
+float KD = 2;
+float KI = 0;
+int proporcional = 0;
+int integral = 0;
+int derivativo = 0;
+int diferencial = 0;
+int ultimoProporcional = 0;
+int setpoint = 350;
+
+
+// Dados Integral //
+int erro1 = 0;
+int erro2 = 0;
+int erro3 = 0;
+int erro4 = 0;
+int erro5 = 0;
+int erro6 = 0;
+
+
+
+int velocidadeRobo = 220;
+int velocidadeFrente = 240;
+int velocidadeAtras = 120;
+void setup() {  
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+
+  
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(IR, OUTPUT);
+
+  digitalWrite(IR, HIGH);
+
+
+  for (int i = 0; i < 8; i++) {
+    pinMode(pinosSensores[i], INPUT);
+    }
+  
+ calibracao();
+    
 }
-
-void setup()
-{
-    // Define os pinos como saídas
-    pinMode(ENA, OUTPUT);
-    pinMode(ENB, OUTPUT);
-    pinMode(in1, OUTPUT);
-    pinMode(in2, OUTPUT);
-    pinMode(in3, OUTPUT);
-    pinMode(in4, OUTPUT);
-
-    Serial.begin(9600);
-    //Emiter PIN do sensor 
-    pinMode(2, OUTPUT);
-    digitalWrite(2, 1);
-
-    // Inicializa o PID
-    Setpoint = 5; // Defina o valor de referência aqui
-
-    // Adiciona uma partida inicial para os motores
-    analogWrite(ENA, 110); // Velocidade inicial para o motor A
-    digitalWrite(in1,LOW);
-    digitalWrite(in2,HIGH);
-    analogWrite(ENB, 110); // Velocidade inicial para o motor B
-    digitalWrite(in3,HIGH);
-    digitalWrite(in4,LOW);
-
-    // delay(2000); // Mantém a velocidade inicial por 2 segundos
-
-    // analogWrite(ENA, 0); // Para o motor A
-    // analogWrite(ENB, 0); // Para o motor B
-}
-
 void loop() {
+  freios();
+  leituraSensores();
+  PID();
 
-    //Leitura dos sensores
-    int L4 = analogRead(A3); //Sensor da direita
-    int L5 = analogRead(A4); //Sensor da esquerda
+  //motores(200, 200);
+  //delay(15);
+  //motores(0,0);
+  //posicaoLinha = leituraSensores();
+  //Serial.println(posicaoLinha);
+ //leituraSensores();
+}
 
-    Input = L5 - L4; // Diferença entre os sensores
-    Compute();
 
-//     // Ajusta o sinal de Output com base no sinal de Input
-//     if (Input < 0 && Output > 0) {
-//       Output = -Output;
-// }
+// leituraSensores() lê todos os valores analógicos dos sensores,
+// e os amarzena na array digital[8] já convertidos em 0 ou 1;
+// Também faz a soma *ponderada* e soma normal dos valores dos sensores,
+// a fim de calcular a posição do robô em relação à linha.
+// Para 8 sensores, por exemplo, definiu-se a posição ideal (robô seguindo o meio da linha) como 350
+// (100 de peso para cada sensor, começando do sensor 0 e indo até o 7)
+int leituraSensores(void) {
+  for (int i = 0; i < 8; i++) {
+    sensores[i] = analogRead(pinosSensores[i]);
+    if(pista==1) { if(sensores[i] >= linha[i]) { digital[i] = 0; } else { digital[i] = 1;}}
+    if(pista==0) { if(sensores[i] >= linha[i]) { digital[i] = 1;} else {digital[i] = 0;}}
+    //Serial.print(digital[i]);
+    //Serial.print(" ");
+    }
+    //delay(15);
+  //Serial.println(" ");
 
-    // Aqui e possivel usar o valor de Output para controlar seus motores
-    int baseSpeed = 125; // Velocidade base dos motores
-    int maxChange = 125; // Máxima alteração permitida na velocidade dos motores
+  somaPonderada = (700*digital[0]) + (600*digital[1]) + (500*digital[2]) + (400*digital[3]) + (300*digital[4]) + (200*digital[5]) + (100*digital[6]) + (0*digital[7]);
+  soma = (digital[0] + digital[1] + digital[2] + digital[3] + digital[4] + digital[5] + digital[6] + digital[7]);
+  posicao = (somaPonderada / soma);
 
-    // Calcula a velocidade de cada motor
-    int motorSpeedA = constrain(baseSpeed - Output, 0, 250);
-    int motorSpeedB = constrain(baseSpeed + Output, 0, 250);
 
-    // Certifica-se de que a velocidade não ultrapasse o máximo permitido
-    motorSpeedA = constrain(motorSpeedA, 0, 125);
-    motorSpeedB = constrain(motorSpeedB, 0, 125);
+// Conserva a última posição lida pelo robô caso os sensores saim completamente da linha
+  if(ultimaPosicao<=100 && posicao==-1) {
+    posicao = 0;
+  }
+   if(ultimaPosicao>=600 && posicao==-1) {
+    posicao = 700;
+  }
 
-    Serial.print("Sensor da direita: ");
-    Serial.print(L4);
-    Serial.print(" Sensor da esquerda: ");
-    Serial.print(L5);
-    Serial.print(" Velocidade A: ");
-    Serial.print(motorSpeedA);
-    Serial.print(" Velocidade B: ");
-    Serial.print(motorSpeedB);
-    Serial.print(" Output: ");
-    Serial.print(Output);
-    Serial.print(" Input :");
-    Serial.println(Input);
+  ultimaPosicao = posicao;
+  return posicao;
+}
 
-   // Define a velocidade dos motores
-   analogWrite(ENA, motorSpeedA);
-   analogWrite(ENB, motorSpeedB);
+
+void motores(int esquerdo, int direito) {
+  if(direito>=0) {
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN1, HIGH);
+    PWMA = IN1;
+  } else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    direito = direito * (-1);
+    PWMA = IN2;
+  }
+  analogWrite(PWMA, direito);
+
+
+  if(esquerdo>=0) {
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    PWMB = IN3;
+  } else {
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    esquerdo = esquerdo * (-1);
+    PWMB = IN4;
+  }
+  analogWrite(PWMB, esquerdo);
+}
+
+// Lê, primeiramente, os valores apenas da pista
+// Depois, lê os valores da linha
+// Faz uma média entre esses valores, resultando na calibração
+// O resultado da calibração é usado no momento de converter os dados analógicos para digitais.
+void calibracao(){
+  digitalWrite(13, HIGH);
+  for (int i = 0; i <= 1000; i++) {
+    calibracaoPista();
+  }
+  digitalWrite(13, LOW);
+  delay(3000);
+  digitalWrite(13, HIGH);
+  for (int i = 0; i <= 1000; i++) {
+    calibracaoLinha();
+  }
+  delay(100);
+  resultadoCalibracao();
+  digitalWrite(13, LOW);
+}
+
+void calibracaoPista() {
+  for (int i = 0; i < 8; i++) {
+    leituraPista[i] = analogRead(pinosSensores[i]);
+    Serial.print(leituraPista[i]);
+    Serial.print(" ");
+    }
+  Serial.println(" ");    
+}
+
+void calibracaoLinha() {
+   for (int i = 0; i < 8; i++) {
+    leituraLinha[i] = analogRead(pinosSensores[i]);
+    Serial.print(leituraLinha[i]);
+    Serial.print(" ");
+    }
+  Serial.println(" ");  
+}
+
+
+void resultadoCalibracao() {
+  for (int i = 0; i < 8; i++) {
+    linha[i] = (leituraPista[i] + leituraLinha[i])/2;
+    Serial.print(linha[i]);
+    Serial.print(" ");
+    }
+  Serial.println(" ");    
+}
+
+
+void PID() {
+  proporcional = posicao - setpoint;
+  derivativo = proporcional - ultimoProporcional;
+  integral = erro1 + erro2 + erro3 + erro4 + erro5 + erro6;
+  ultimoProporcional = proporcional;
+  erro6 = erro5;
+  erro5 = erro4;
+  erro4 = erro3;
+  erro3 = erro2;
+  erro2 = erro1;
+  erro1 = proporcional;
+  int diferencial = (proporcional * KP) + (derivativo * KD) + (integral * KI);
+  if (diferencial > velocidadeRobo) diferencial = velocidadeRobo;
+  else if (diferencial < -velocidadeRobo) diferencial = -velocidadeRobo;
+  (diferencial < 0) ?
+  motores(velocidadeRobo, velocidadeRobo + diferencial) : motores(velocidadeRobo - diferencial, velocidadeRobo);
+}
+
+void freios() {
+  if (posicao <= 100) {
+    motores(-velocidadeAtras, velocidadeFrente);
+  }
+  if (posicao >= 600) {
+    motores(velocidadeFrente, -velocidadeAtras);
+  }
 }
